a8575abc4d0815b8e31bc15bc649f9df7d24199e
hamza01syed <127969573+hamza01syed@users.noreply.github.com>
2024-03-19 13:35:32 -0700

    index.js file updated

--- index.js
+++ index.js
@@ -1,30 +1,29 @@
 #! /usr/bin/env node
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var inquirer_1 = require("inquirer");
-var answer = await inquirer_1.default.prompt([
-    { message: "Enter first number", type: "number", name: "firstNumber" },
-    { message: "Enter 2nd Number", type: "number", name: "secondNumber" },
-    {
-        message: "Select one of the operators to perform action",
-        type: "list",
-        name: "operator",
-        choices: ["addition", "subtraction", "multiplication", "division"],
-    },
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your 2nd Number", type: "number", name: "secondNumber" },
+  {
+    message: "Select one of the operators to perform action",
+    type: "list",
+    name: "operator",
+    choices: ["addition", "subtraction", "multiplication", "division"],
+  },
 ]);
 // console.log(answer);
-if (answer.operator === "addition") {
-    console.log(answer.firstNumber + answer.secondNumber);
+if(answer.operator==="addition"){
+    console.log(answer.firstNumber+answer.secondNumber)
 }
-else if (answer.operator === "subtraction") {
-    console.log(answer.firstNumber - answer.secondNumber);
+else if(answer.operator==="subtraction"){
+    console.log(answer.firstNumber-answer.secondNumber)
 }
-else if (answer.operator === "multiplication") {
-    console.log(answer.firstNumber * answer.secondNumber);
+else if(answer.operator==="multiplication"){
+    console.log(answer.firstNumber*answer.secondNumber)
 }
-else if (answer.operator === "division") {
-    console.log(answer.firstNumber / answer.secondNumber);
+else if(answer.operator==="division"){
+    console.log(answer.firstNumber/answer.secondNumber)
+}else{
+    console.log("please select correct operator")
 }
-else {
-    console.log("please select correct operator");
-}
+
