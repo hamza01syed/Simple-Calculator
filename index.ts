b9c3df9de79ddc77fd3bf4c3b7daef61135d56b5
hamza01syed <127969573+hamza01syed@users.noreply.github.com>
2024-03-19 12:18:13 -0700

    text edited

--- index.ts
+++ index.ts
@@ -2,8 +2,8 @@
 import inquirer from "inquirer";
 
 const answer = await inquirer.prompt([
-  { message: "Enter first number", type: "number", name: "firstNumber" },
-  { message: "Enter 2nd Number", type: "number", name: "secondNumber" },
+  { message: "Enter your first number", type: "number", name: "firstNumber" },
+  { message: "Enter your 2nd Number", type: "number", name: "secondNumber" },
   {
     message: "Select one of the operators to perform action",
     type: "list",
